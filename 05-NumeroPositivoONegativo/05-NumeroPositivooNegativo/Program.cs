03621de464676249bacced8714bac1a2daf3b1b4
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:23:29 -0300

    Add files via upload

---
+++ 05-NumeroPositivoONegativo/05-NumeroPositivooNegativo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05_NumeroPositivooNegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Introducir un numero al azar, distinto de cero, para determinar si es positivo o negativo: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            while (a == 0)
+            {
+                Console.WriteLine("Numero introducido incorrecto, debe ser distinto de cero:");
+                a = Convert.ToInt32(Console.ReadLine());
+
+            }
+            if (a > 0)
+            {
+                Console.WriteLine("El numero es positivo");
+            }
+            if (a < 0)
+            {
+                Console.WriteLine("El numero es negativo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
